64f09ff81edb7ef518010d1e3ab8db9c431fe0ed
Corbin Simpson <MostAwesomeDude@gmail.com>
2009-09-29 04:12:06 -0700

    Add scale demo.

---
+++ demos/scale.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+
+import fluidsynth
+
+settings = fluidsynth.FluidSettings()
+settings.quality = "low"
+
+synth = fluidsynth.FluidSynth(settings)
+synth.load_soundfont("double.sf2")
+
+driver = fluidsynth.FluidAudioDriver(settings, synth)
+
+player = fluidsynth.FluidPlayer(synth)
+
+scale = (60, 62, 64, 65, 67, 69, 71, 72)
+
+for i in scale:
+    synth.noteon(0, i, 127)
+    time.sleep(0.5)
+    synth.noteoff(0, i)
+
+for i in reversed(scale):
+    synth.noteon(0, i, 127)
+    time.sleep(0.5)
+    synth.noteoff(0, i)
