bb5e366e72ae9dff040d3844d0815d623842ef84
Corbin Simpson <MostAwesomeDude@gmail.com>
2009-09-29 04:43:51 -0700

    Add setup.py.
    Kind of surprised this was missing.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+    cmdclass = {"build_ext": build_ext},
+    ext_modules = [
+        Extension("fluidsynth", ["fluidsynth.pyx"],
+            libraries=["fluidsynth"]
+        )
+    ]
+)
