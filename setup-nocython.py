18a8fab98943b1d8af8561c94fdfbc3fd57706cd
Corbin Simpson <MostAwesomeDude@gmail.com>
2009-10-05 01:52:20 -0700

    Win32 support.
    I decided to include the release version of the library only; re-linking
    with fluidsynth_lib_debug is completely possible.
    
    You'll need Dx SDK in order to build this whole cacophonous shebang.
    
    Also I hate MSVC so very very very much.

--- setup-nocython.py
+++ setup-nocython.py
@@ -5,7 +5,8 @@ from distutils.core import setup, Extension
 setup(
     ext_modules = [
         Extension("fluidsynth", ["fluidsynth.c"],
-            libraries=["fluidsynth"]
+            libraries=["fluidsynth_lib", "dsound", "winmm", "user32"],
+            library_dirs=["C:/Program Files/Microsoft DirectX SDK (August 2006)/Lib/x86"],
         )
     ]
 )
