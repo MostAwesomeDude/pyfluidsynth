7eb01bb9a83fed756e82a900fdcbad86ee3ca1d6
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-02-28 02:14:49 -0800

    Finish porting bindings.
    Not quite all working yet.

--- fluidsynth/_bindings.py
+++ fluidsynth/_bindings.py
@@ -1,4 +1,5 @@
-from ctypes import cdll, c_char_p, c_double, c_int, c_void_p
+from ctypes import (CFUNCTYPE, cdll, c_char_p, c_double, c_int, c_uint,
+    c_short, c_void_p)
 
 # The shared object we're gonna use.
 handle = cdll.LoadLibrary("libfluidsynth.so.1")
@@ -33,3 +34,144 @@ handle.fluid_settings_setint.restype = c_int
 
 handle.fluid_settings_setstr.argtypes = (c_void_p, c_char_p, c_char_p)
 handle.fluid_settings_setstr.restype = c_int
+
+# From synth.h
+handle.new_fluid_synth.argtypes = (c_void_p,)
+handle.new_fluid_synth.restype = c_void_p
+
+handle.delete_fluid_synth.argtypes = (c_void_p,)
+handle.delete_fluid_synth.restype = None
+
+handle.fluid_synth_sfload.argtypes = (c_void_p, c_char_p, c_int)
+handle.fluid_synth_sfload.restype = c_int
+
+handle.fluid_synth_sfreload.argtypes = (c_void_p, c_uint)
+handle.fluid_synth_sfreload.restype = c_int
+
+handle.fluid_synth_sfunload.argtypes = (c_void_p, c_uint, c_int)
+handle.fluid_synth_sfunload.restype = c_int
+
+handle.fluid_synth_noteon.argtypes = (c_void_p, c_int, c_int, c_int)
+handle.fluid_synth_noteon.restype = c_int
+
+handle.fluid_synth_noteoff.argtypes = (c_void_p, c_int, c_int)
+handle.fluid_synth_noteoff.restype = c_int
+
+handle.fluid_synth_cc.argtypes = (c_void_p, c_int, c_int, c_int)
+handle.fluid_synth_cc.restype = c_int
+
+handle.fluid_synth_pitch_bend.argtypes = (c_void_p, c_int, c_int)
+handle.fluid_synth_pitch_bend.restype = c_int
+
+handle.fluid_synth_pitch_wheel_sens.argtypes = (c_void_p, c_int, c_int)
+handle.fluid_synth_pitch_wheel_sens.restype = c_int
+
+handle.fluid_synth_program_change.argtypes = (c_void_p, c_int, c_int)
+handle.fluid_synth_program_change.restype = c_int
+
+handle.fluid_synth_bank_select.argtypes = (c_void_p, c_int, c_int)
+handle.fluid_synth_bank_select.restype = c_int
+
+# From audio.h
+handle.new_fluid_audio_driver.argtypes = (c_void_p, c_void_p)
+handle.new_fluid_audio_driver.restype = c_void_p
+
+handle.delete_fluid_audio_driver.argtypes = (c_void_p,)
+handle.delete_fluid_audio_driver.restype = None
+
+# From midi.h
+handle.new_fluid_player.argtypes = (c_void_p,)
+handle.new_fluid_player.restype = c_void_p
+
+handle.delete_fluid_player.argtypes = (c_void_p,)
+handle.delete_fluid_player.restype = c_int
+
+handle.fluid_player_add.argtypes = (c_char_p, c_char_p)
+handle.fluid_player_add.restype = c_int
+
+handle.fluid_player_play.argtypes = (c_char_p,)
+handle.fluid_player_play.restype = c_int
+
+handle.fluid_player_stop.argtypes = (c_char_p,)
+handle.fluid_player_stop.restype = c_int
+
+handle.fluid_player_join.argtypes = (c_char_p,)
+handle.fluid_player_join.restype = c_int
+
+# From event.h
+handle.new_fluid_event.argtypes = ()
+handle.new_fluid_event.restype = c_void_p
+
+handle.delete_fluid_event.argtypes = (c_void_p,)
+handle.delete_fluid_event.restype = None
+
+handle.fluid_event_timer.argtypes = (c_void_p, c_void_p)
+handle.fluid_event_timer.restype = None
+
+handle.fluid_event_note.argtypes = (c_void_p, c_int, c_short, c_short, c_uint)
+handle.fluid_event_note.restype = None
+
+handle.fluid_event_noteon.argtypes = (c_void_p, c_int, c_short, c_short)
+handle.fluid_event_noteon.restype = None
+
+handle.fluid_event_noteoff.argtypes = (c_void_p, c_int, c_short)
+handle.fluid_event_noteoff.restype = None
+
+handle.fluid_event_get_source.argtypes = (c_void_p,)
+handle.fluid_event_get_source.restype = c_short
+
+handle.fluid_event_set_source.argtypes = (c_void_p, c_short)
+handle.fluid_event_set_source.restype = None
+
+handle.fluid_event_get_dest.argtypes = (c_void_p,)
+handle.fluid_event_get_dest.restype = c_short
+
+handle.fluid_event_set_dest.argtypes = (c_void_p, c_short)
+handle.fluid_event_set_dest.restype = None
+
+# From seq.h
+fluid_event_callback_t = CFUNCTYPE(None, c_uint, c_void_p, c_void_p, c_void_p)
+
+handle.new_fluid_sequencer.argtypes = ()
+handle.new_fluid_sequencer.restype = c_void_p
+
+handle.delete_fluid_sequencer.argtypes = (c_void_p,)
+handle.delete_fluid_sequencer.restype = None
+
+handle.fluid_sequencer_count_clients.argtypes = (c_void_p,)
+handle.fluid_sequencer_count_clients.restype = c_int
+
+handle.fluid_sequencer_get_client_id.argtypes = (c_void_p, c_int)
+handle.fluid_sequencer_get_client_id.restype = c_int
+
+handle.fluid_sequencer_get_client_name.argtypes = (c_void_p, c_int)
+handle.fluid_sequencer_get_client_name.restype = c_char_p
+
+handle.fluid_sequencer_client_is_dest.argtypes = (c_void_p, c_int)
+handle.fluid_sequencer_client_is_dest.restype = c_int
+
+handle.fluid_sequencer_register_client.argtypes = (c_void_p, c_char_p,
+    fluid_event_callback_t, c_void_p)
+handle.fluid_sequencer_register_client.restype = c_short
+
+handle.fluid_sequencer_unregister_client.argtypes = (c_void_p, c_short)
+handle.fluid_sequencer_unregister_client.restype = None
+
+handle.fluid_sequencer_send_now.argtypes = (c_void_p, c_void_p)
+handle.fluid_sequencer_send_now.restype = None
+
+handle.fluid_sequencer_send_at.argtypes = (c_void_p, c_void_p, c_uint, c_int)
+handle.fluid_sequencer_send_at.restype = c_int
+
+handle.fluid_sequencer_get_time_scale.argtypes = (c_void_p,)
+handle.fluid_sequencer_get_time_scale.restype = c_double
+
+handle.fluid_sequencer_set_time_scale.argtypes = (c_void_p, c_double)
+handle.fluid_sequencer_set_time_scale.restype = None
+
+handle.fluid_sequencer_get_tick.argtypes = (c_void_p,)
+handle.fluid_sequencer_get_tick.restype = c_uint
+
+# From seqbind.h
+handle.fluid_sequencer_register_fluidsynth.argtypes = (c_void_p, c_void_p)
+handle.fluid_sequencer_register_fluidsynth.restype = c_short
--- fluidsynth/fluidsynth.py
+++ fluidsynth/fluidsynth.py
@@ -85,3 +85,226 @@ class FluidSettings(object):
             self["synth.chorus.active"] = "on"
             self["synth.reverb.active"] = "on"
             self["synth.sample-rate"] = 44100
+
+class FluidSynth(object):
+
+    def __init__(self, settings):
+        self._sf_dict = {}
+        self.synth = handle.new_fluid_synth(settings.settings)
+
+    def __del__(self):
+        failed = []
+        for sf in self._sf_dict:
+            if handle.fluid_synth_sfunload(self.synth, self._sf_dict[sf],
+                True):
+                failed.append(sf)
+        handle.delete_fluid_synth(self.synth)
+
+        if failed:
+            raise FluidError("Couldn't unload soundfonts: %s" % failed)
+
+    def load_soundfont(self, sf, reload_presets=True):
+        if sf in self._sf_dict:
+            if (handle.fluid_synth_sfreload(self.synth, self._sf_dict[sf])
+                == -1):
+                raise FluidError("Couldn't reload soundfont %s" % sf)
+        else:
+            i = handle.fluid_synth_sfload(self.synth, sf, reload_presets)
+            if i == -1:
+                raise FluidError, "Couldn't load soundfont %s" % sf
+            else:
+                self._sf_dict[sf] = i
+
+    def unload_soundfont(self, sf, reload_presets=True):
+        if sf not in self._sf_dict:
+            raise FluidError("Soundfont %s never loaded" % sf)
+        if handle.fluid_synth_sfunload(self.synth, self._sf_dict[sf],
+            reload_presets):
+            raise FluidError("Couldn't unload soundfont %s" % sf)
+        else:
+            del self._sf_dict[sf]
+
+    def noteon(self, channel, pitch, velocity):
+        if isinstance(velocity, float):
+            velocity = int(velocity * 127)
+        handle.fluid_synth_noteon(self.synth, channel, pitch, velocity)
+
+    def noteoff(self, channel, pitch):
+        handle.fluid_synth_noteoff(self.synth, channel, pitch)
+
+    def cc(self, channel, control, value):
+        handle.fluid_synth_cc(self.synth, channel, control, value)
+
+    control_change = cc
+
+    def pitch_bend(self, channel, value):
+        handle.fluid_synth_pitch_bend(self.synth, channel, value)
+
+    def pitch_wheel_sens(self, channel, value):
+        handle.fluid_synth_pitch_wheel_sens(self.synth, channel, value)
+
+    pitch_wheel_sensitivity = pitch_wheel_sens
+
+    def program_change(self, channel, program):
+        handle.fluid_synth_program_change(self.synth, channel, program)
+
+    def bank_select(self, channel, bank):
+        handle.fluid_synth_bank_select(self.synth, channel, bank)
+
+class FluidAudioDriver(object):
+    def __init__(self, settings, synth):
+        self.audio_driver = handle.new_fluid_audio_driver(settings.settings,
+            synth.synth)
+
+    def __del__(self):
+        handle.delete_fluid_audio_driver(self.audio_driver)
+
+class FluidPlayer(object):
+
+    paused = True
+
+    def __init__(self, synth):
+        self.player = handle.new_fluid_player(synth.synth)
+
+    def __del__(self):
+        self.stop()
+        self.join()
+
+        if handle.delete_fluid_player(self.player):
+            raise FluidError("Couldn't delete fluid player!")
+
+    def add(self, midi):
+        handle.fluid_player_add(self.player, midi)
+
+    def play(self, midi=None):
+        if midi:
+            self.add(midi)
+        handle.fluid_player_play(self.player)
+        self.paused = False
+
+    def stop(self):
+        handle.fluid_player_stop(self.player)
+        self.paused = True
+
+    def join(self):
+        handle.fluid_player_join(self.player)
+
+    def pause(self):
+        self.play() if self.paused else self.stop()
+        self.paused = not self.paused
+
+class FluidEvent(object):
+
+    source = -1
+    dest = -1
+
+    def __init__(self):
+        self.event = handle.new_fluid_event()
+
+    def __del__(self):
+        handle.delete_fluid_event(self.event)
+
+    @property
+    def source(self):
+        return handle.fluid_event_get_source(self.event)
+
+    @source.setter
+    def source(self, value):
+        handle.fluid_event_set_source(self.event, value)
+
+    @property
+    def dest(self):
+        return handle.fluid_event_get_dest(self.event)
+
+    @dest.setter
+    def dest(self, value):
+        handle.fluid_event_set_dest(self.event, value)
+
+    def timer(self):
+        # XXX should support callbacks
+        handle.fluid_event_timer(self.event, None)
+
+    def note(self, channel, key, velocity, duration):
+        handle.fluid_event_note(self.event, channel, key, velocity, duration)
+
+    def noteon(self, channel, key, velocity):
+        handle.fluid_event_noteon(self.event, channel, key, velocity)
+
+    def noteoff(self, channel, key):
+        handle.fluid_event_noteoff(self.event, channel, key)
+
+class FluidSequencer(dict):
+
+    _bpm = 120
+    _tpb = 120
+
+    def __init__(self, *synths):
+        super(FluidSequencer, self).__init__()
+
+        self.seq = handle.new_fluid_sequencer()
+
+        if synths:
+            for synth in synths:
+                self.add_synth(synth)
+
+        self._update_tps()
+
+    def __del__(self):
+        handle.delete_fluid_sequencer(self.seq)
+
+    def __delitem__(self, key):
+        id, name = self[key]
+        handle.fluid_sequencer_unregister_client(self.seq, id)
+
+        super(FluidSequencer, self).__delitem__(key)
+
+    @property
+    def beats_per_minute(self):
+        return self._bpm
+
+    @beats_per_minute.setter
+    def beats_per_minute(self, value):
+        self._bpm = value
+        self._update_tps()
+
+    @property
+    def ticks_per_beat(self):
+        return self._tpb
+
+    @ticks_per_beat.setter
+    def ticks_per_beat(self, value):
+        self._tpb = value
+        self._update_tps()
+
+    @property
+    def ticks_per_second(self):
+        return handle.fluid_sequencer_get_time_scale(self.seq)
+
+    @ticks_per_second.setter
+    def ticks_per_second(self, value):
+        handle.fluid_sequencer_set_time_scale(self.seq, value)
+
+    @property
+    def ticks(self):
+        return handle.fluid_sequencer_get_tick(self.seq)
+
+    def _update_tps(self):
+        self.ticks_per_second = (self._tpb * self._bpm) / 60.0
+
+    def is_dest(self, id):
+        return bool(handle.fluid_sequencer_client_is_dest(self.seq, id))
+
+    def add_synth(self, synth):
+        id = handle.fluid_sequencer_register_fluidsynth(self.seq, synth.synth)
+        name = handle.fluid_sequencer_get_client_name(self.seq, id)
+
+        self[synth] = (id, name)
+
+        return (id, name)
+
+    def send(self, event, timestamp, absolute=True):
+        handle.fluid_sequencer_send_at(self.seq, event.event, timestamp,
+            absolute)
+
+    def send_right_now(self, event):
+        handle.fluid_sequencer_send_now(self.seq, event.event)
