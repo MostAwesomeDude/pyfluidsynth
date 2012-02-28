2509136445fdcd9b6703bf5f7eaede8e3e31ac5e
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-02-28 02:33:40 -0800

    setup: Cleanup.

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+from distutils.core import setup
 
 setup(
-    name="pyfluidsynth",
-    version="0.1",
+    name="fluidsynth",
+    version="0.2",
     description="Fluidsynth bindings",
     author="Corbin Simpson",
     author_email="MostAwesomeDude@gmail.com",
     url="http://corbinsimpson.com/",
-    ext_modules = [
-        Extension("fluidsynth", ["fluidsynth.c"],
-            libraries=["fluidsynth_lib", "dsound", "winmm", "user32"],
-            library_dirs=["C:/Program Files/Microsoft DirectX SDK (August 2006)/Lib/x86"],
-        )
-    ]
+    packages=["fluidsynth"],
 )
